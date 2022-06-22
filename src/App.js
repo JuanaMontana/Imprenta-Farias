781e5c63d28477fbd55cc4a72314beb3f89c4f89
Juan Farias <juanjo.farias.90@gmail.com>
2022-06-21 23:47:50 -0400

    bootstrap y navbar
    import bootstrap y navbar de prueba

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <div className="App">
+      <Navbar>
+  <Container>
+    <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+    <Navbar.Toggle />
+    <Navbar.Collapse className="justify-content-end">
+      <Navbar.Text>
+        Signed in as: <a href="#login">Mark Otto</a>
+      </Navbar.Text>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
