65d3da97aaf92574df27d3f23256a48291bb56b4
Juan Farias <juanjo.farias.90@gmail.com>
2022-06-27 04:15:34 -0400

    imagen, logo y favicon
    se agrega imagen al navbar, se agrega loco favicon y titulo de pagina

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,30 @@
+import fotito from '../img/BLlogo-printmey-Completo.png';
+
+
 const NavBar = () => {
     return (
         <nav class="navbar-expand-lg navbar-light bg-light ">
+
+           
+            
+<img src={fotito} alt="..." height="150"/>
             <div class="container-fluid text-center">
-                <a class="navbar-brand">PRINTMEY</a>
+            
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-center" id="navbarText">
                     <ul class="navbar-nav justify-content-center">
-                        <li class="nav-item">
+                        <li class="nav-item m-4">
                             <a class="nav-link active" aria-current="page" href="#">Inicio</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item m-4">
                             <a class="nav-link" href="#">Tienda</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item m-4">
                             <a class="nav-link" href="#">Ofertas</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item m-4">
                             <a class="nav-link" href="#">¿Quienes somos?</a>
                         </li>
                     </ul>
