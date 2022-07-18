573de12fe32bcfc30283f06dc62e67a488484dd1
Juan Farias <juanjo.farias.90@gmail.com>
2022-07-18 17:54:08 -0400

    promise implementada
    se implementa la simulacion de peticion a la base de datos, seteado en 1000ms

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,33 @@
-import React from 'react'
-import ItemCount from './ItemCount'
-import ItemList from './ItemList'
-import products from '../products.json'
+import React, { useState, useEffect } from "react";
+import ItemList from "./ItemList";
+import Products from "../products.json";
+
+const ItemListContainer = () => {
+  const [products,setProducts] = useState([]);
+
+  useEffect(() => {
+    const promesa = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const items = Products;
+
+        resolve(items);
+      }, 1000);
+    });
+
+    promesa.then((param) => {
+      console.log({ param });
+      setProducts(param);
+    });
+  }, []);
+
+  return (
+    <div class="row justify-content-md-center">
+      <ItemList items={products} />
+    </div>
+  );
+};
+
+export default ItemListContainer;
 
 /*const ItemListContainer = new Promise((resolve, reject) => {
   setTimeout(() => {
@@ -20,15 +46,12 @@ ItemListContainer.then((param) => {
       )
 });*/
 
-
-const ItemListContainer = ({ grettings }) => {
+/*const ItemListContainer = ({ grettings }) => {
   return (
     <div class="row justify-content-md-center">{grettings = "este es el grettings"}
       <ItemList items={products}/>
     </div>
   )
-}
-
-export default ItemListContainer
+}*/
 
 
