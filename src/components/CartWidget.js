55a88c7b9d982d8863adf61cf65c85dc2253c6cc
Juan Farias <juanjo.farias.90@gmail.com>
2022-08-02 23:35:57 -0400

    cartwidget no se muestra si es 0
    se borra el numero de cart widget si es que es 0

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,25 +1,24 @@
 import React, { useContext, useEffect , useState } from "react";
 import { CartContext } from "../context/useContext";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const CartWidget = () => {
   const { Conteo } = useContext(CartContext);
   const navigate = useNavigate();
 
-  
+
 
 
   
   return (
     <div>
-      
-    
 
-      <button className="btn btn-outline-light mt-5 mb-5 me-5 " onClick={() => navigate("/cart")}>
+<div className="btn btn-outline-light mt-5 mb-5 me-5 ">
+            <Link to="/Cart" class="navbar-brand" href="#">
+            <i style={{color:"white"}}>
 
-      {Conteo()} 
-
-        <svg
+            <svg
           xmlns="http://www.w3.org/2000/svg"
           width="26"
           height="26"
@@ -35,10 +34,14 @@ const CartWidget = () => {
           <circle cx="20" cy="21" r="1"></circle>
           <path d="M1 1h4l2.68 13.39a2 2 0 002 1.61h9.72a2 2 0 002-1.61L23 6H6"></path>
         </svg>
-        
-
-      </button>
-
+             <span className="span-icon ms-3" style={{color:"white"}}>{Conteo() === 0 ? "" : Conteo()}</span>
+            
+            </i>
+            </Link>
+            
+        </div>
+      
+    
     </div>
   );
 };
